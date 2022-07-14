706d45c36253896de2b91822bf51e801974408c6
SI-86 <samaldenhren@gmail.com>
2022-07-14 13:58:09 -0400

    moved to twitch auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-import CredentialsProvider from "next-auth/providers/credentials";
+import TwitchProvider from "next-auth/providers/twitch";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -8,29 +7,14 @@ import { prisma } from "../../../server/db/client";
 import { env } from "../../../server/env";
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        name: {
-          label: "Name",
-          type: "text",
-          placeholder: "Enter your name",
-        },
-      },
-      async authorize(credentials, _req) {
-        const user = { id: 1, name: credentials?.name ?? "J Smith" };
-        return user;
-      },
-    }),
-  ],
+    // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        TwitchProvider({
+            clientId: env.TWITCH_CLIENT_ID,
+            clientSecret: env.TWITCH_CLIENT_SECRET,
+        }),
+    ],
 };
 
 export default NextAuth(authOptions);
