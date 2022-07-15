327c5f2bcf7bc4d3c1dd5bbbe2dd07cf6a8b2264
SI-86 <samaldenhren@gmail.com>
2022-07-14 23:07:48 -0400

    nextauth_url not req

--- src/server/env-schema.js
+++ src/server/env-schema.js
@@ -4,7 +4,7 @@ const envSchema = z.object({
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET: z.string(),
-    NEXTAUTH_URL: z.string().url(),
+    // NEXTAUTH_URL: z.string().url(),
     TWITCH_CLIENT_ID: z.string(),
     TWITCH_CLIENT_SECRET: z.string(),
 
